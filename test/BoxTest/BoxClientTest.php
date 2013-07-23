97c6fb55419666dbdc206c3d3bc6c587eeace75e
chance garcia <develop@chancegarcia.com>
2013-07-23 15:55:05 -0400

    attempt zf2 phpunit bootstrap

---
+++ test/BoxTest/BoxClientTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: chance
+ * Date: 7/23/13
+ * Time: 2:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace BoxTest;
+use BoxTest\Bootstrap;
+
+
+class BoxClientTest extends \PHPUnit_Framework_TestCase {
+
+    public function testClient()
+    {
+        $this->assertTrue(true);
+    }
+}
